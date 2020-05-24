73280a90f6b9885434c20cf850c1a7037c408aca
Lakshay <47170073+lakshaysinghwal@users.noreply.github.com>
2020-05-24 16:10:41 +0530

    python file

--- gui.py
+++ gui.py
@@ -14,8 +14,6 @@ class ChatInterface(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
-
-        # sets default bg for top level windows
         self.tl_bg = "#EEEEEE"
         self.tl_bg2 = "#EEEEEE"
         self.tl_fg = "#000000"
@@ -23,23 +21,12 @@ class ChatInterface(Frame):
 
         menu = Menu(self.master)
         self.master.config(menu=menu, bd=5)
-        # Menu bar
-
-        # File
         file = Menu(menu, tearoff=0)
         menu.add_cascade(label="File", menu=file)
-        # file.add_command(label="Save Chat Log", command=self.save_chat)
         file.add_command(label="Clear Chat", command=self.clear_chat)
-        #  file.add_separator()
         file.add_command(label="Exit", command=self.chatexit)
-
-        # Options
         options = Menu(menu, tearoff=0)
         menu.add_cascade(label="Options", menu=options)
-
-        # username
-
-        # font
         font = Menu(options, tearoff=0)
         options.add_cascade(label="Font", menu=font)
         font.add_command(label="Default", command=self.font_change_default)
@@ -47,53 +34,32 @@ class ChatInterface(Frame):
         font.add_command(label="System", command=self.font_change_system)
         font.add_command(label="Helvetica", command=self.font_change_helvetica)
         font.add_command(label="Fixedsys", command=self.font_change_fixedsys)
-
-        # color theme
         color_theme = Menu(options, tearoff=0)
         options.add_cascade(label="Color Theme", menu=color_theme)
         color_theme.add_command(label="Default", command=self.color_theme_default)
-        # color_theme.add_command(label="Night",command=self.)
         color_theme.add_command(label="Grey", command=self.color_theme_grey)
         color_theme.add_command(label="Blue", command=self.color_theme_dark_blue)
-
         color_theme.add_command(label="Torque", command=self.color_theme_turquoise)
         color_theme.add_command(label="Hacker", command=self.color_theme_hacker)
-        # color_theme.add_command(label='Mkbhd',command=self.MKBHD)
-
         help_option = Menu(menu, tearoff=0)
         menu.add_cascade(label="Help", menu=help_option)
-        # help_option.add_command(label="Features", command=self.features_msg)
         help_option.add_command(label="About Lakshay's Bot", command=self.msg)
         help_option.add_command(label="Develpoers", command=self.about)
-
         self.text_frame = Frame(self.master, bd=6)
         self.text_frame.pack(expand=True, fill=BOTH)
-
-        # scrollbar for text box
         self.text_box_scrollbar = Scrollbar(self.text_frame, bd=0)
         self.text_box_scrollbar.pack(fill=Y, side=RIGHT)
-
-        # contains messages
         self.text_box = Text(self.text_frame, yscrollcommand=self.text_box_scrollbar.set, state=DISABLED,
                              bd=1, padx=6, pady=6, spacing3=8, wrap=WORD, bg=None, font="Verdana 10", relief=GROOVE,
                              width=10, height=1)
         self.text_box.pack(expand=True, fill=BOTH)
         self.text_box_scrollbar.config(command=self.text_box.yview)
-
-        # frame containing user entry field
         self.entry_frame = Frame(self.master, bd=1)
         self.entry_frame.pack(side=LEFT, fill=BOTH, expand=True)
-
-        # entry field
         self.entry_field = Entry(self.entry_frame, bd=1, justify=LEFT)
         self.entry_field.pack(fill=X, padx=6, pady=6, ipady=3)
-        # self.users_message = self.entry_field.get()
-
-        # frame containing send button and emoji button
         self.send_button_frame = Frame(self.master, bd=0)
         self.send_button_frame.pack(fill=BOTH)
-
-        # send button
         self.send_button = Button(self.send_button_frame, text="Send", width=5, relief=GROOVE, bg='white',
                                   bd=1, command=lambda: self.send_message_insert(None), activebackground="#FFFFFF",
                                   activeforeground="#000000")
@@ -101,12 +67,9 @@ class ChatInterface(Frame):
         self.master.bind("<Return>", self.send_message_insert)
 
         self.last_sent_label(date="No messages sent.")
-        # t2 = threading.Thread(target=self.send_message_insert(, name='t1')
-        # t2.start()
 
     def playResponce(self, responce):
         x = pyttsx3.init()
-        # print(responce)
         li = []
         if len(responce) > 100:
             if responce.find('--') == -1:
@@ -117,8 +80,6 @@ class ChatInterface(Frame):
         x.setProperty('volume', 100)
         x.say(responce)
         x.runAndWait()
-        # print("Played Successfully......")
-
     def last_sent_label(self, date):
 
         try:
@@ -203,14 +164,10 @@ class ChatInterface(Frame):
         self.entry_field.config(bg="#FFFFFF", fg="#000000", insertbackground="#000000")
         self.send_button_frame.config(bg="#EEEEEE")
         self.send_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
-        # self.emoji_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
         self.sent_label.config(bg="#EEEEEE", fg="#000000")
-
         self.tl_bg = "#FFFFFF"
         self.tl_bg2 = "#EEEEEE"
         self.tl_fg = "#000000"
-
-    # Dark
     def color_theme_dark(self):
         self.master.config(bg="#2a2b2d")
         self.text_frame.config(bg="#2a2b2d")
@@ -219,14 +176,10 @@ class ChatInterface(Frame):
         self.entry_field.config(bg="#212121", fg="#FFFFFF", insertbackground="#FFFFFF")
         self.send_button_frame.config(bg="#2a2b2d")
         self.send_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
-        # self.emoji_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
         self.sent_label.config(bg="#2a2b2d", fg="#FFFFFF")
-
         self.tl_bg = "#212121"
         self.tl_bg2 = "#2a2b2d"
         self.tl_fg = "#FFFFFF"
-
-    # Grey
     def color_theme_grey(self):
         self.master.config(bg="#444444")
         self.text_frame.config(bg="#444444")
@@ -235,13 +188,10 @@ class ChatInterface(Frame):
         self.entry_field.config(bg="#4f4f4f", fg="#ffffff", insertbackground="#ffffff")
         self.send_button_frame.config(bg="#444444")
         self.send_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
-        # self.emoji_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
         self.sent_label.config(bg="#444444", fg="#ffffff")
-
         self.tl_bg = "#4f4f4f"
         self.tl_bg2 = "#444444"
         self.tl_fg = "#ffffff"
-
     def color_theme_turquoise(self):
         self.master.config(bg="#003333")
         self.text_frame.config(bg="#003333")
@@ -250,15 +200,10 @@ class ChatInterface(Frame):
         self.entry_field.config(bg="#669999", fg="#FFFFFF", insertbackground="#FFFFFF")
         self.send_button_frame.config(bg="#003333")
         self.send_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
-        # self.emoji_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
         self.sent_label.config(bg="#003333", fg="#FFFFFF")
-
         self.tl_bg = "#669999"
         self.tl_bg2 = "#003333"
         self.tl_fg = "#FFFFFF"
-
-        # Blue
-
     def color_theme_dark_blue(self):
         self.master.config(bg="#263b54")
         self.text_frame.config(bg="#263b54")
@@ -267,14 +212,10 @@ class ChatInterface(Frame):
         self.entry_field.config(bg="#1c2e44", fg="#FFFFFF", insertbackground="#FFFFFF")
         self.send_button_frame.config(bg="#263b54")
         self.send_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
-        # self.emoji_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
         self.sent_label.config(bg="#263b54", fg="#FFFFFF")
-
         self.tl_bg = "#1c2e44"
         self.tl_bg2 = "#263b54"
         self.tl_fg = "#FFFFFF"
-
-    # Torque
     def color_theme_turquoise(self):
         self.master.config(bg="#003333")
         self.text_frame.config(bg="#003333")
@@ -283,14 +224,10 @@ class ChatInterface(Frame):
         self.entry_field.config(bg="#669999", fg="#FFFFFF", insertbackground="#FFFFFF")
         self.send_button_frame.config(bg="#003333")
         self.send_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
-        # self.emoji_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
         self.sent_label.config(bg="#003333", fg="#FFFFFF")
-
         self.tl_bg = "#669999"
         self.tl_bg2 = "#003333"
         self.tl_fg = "#FFFFFF"
-
-    # Hacker
     def color_theme_hacker(self):
         self.master.config(bg="#0F0F0F")
         self.text_frame.config(bg="#0F0F0F")
@@ -299,23 +236,15 @@ class ChatInterface(Frame):
         self.entry_field.config(bg="#0F0F0F", fg="#33FF33", insertbackground="#33FF33")
         self.send_button_frame.config(bg="#0F0F0F")
         self.send_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
-        # self.emoji_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
         self.sent_label.config(bg="#0F0F0F", fg="#33FF33")
-
         self.tl_bg = "#0F0F0F"
         self.tl_bg2 = "#0F0F0F"
         self.tl_fg = "#33FF33"
-
-    # Default font and color theme
     def default_format(self):
         self.font_change_default()
         self.color_theme_default()
-
-
 root = Tk()
-
 a = ChatInterface(root)
 root.geometry(window_size)
 root.title("Lakshay's Bot")
-#root.iconbitmap('i.ico')
 root.mainloop()
